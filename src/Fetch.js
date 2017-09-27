12b5ae61c4e336374e60026d8ea4eb34266616bb
Masato Bito <masato@bz2.jp>
2017-09-27 20:34:42 +0900

    improve sync() method

--- src/Fetch.js
+++ src/Fetch.js
@@ -4,6 +4,9 @@ function fetchAll() {
 
 
 var Fetch = function Fetch() {
+  this.row = 1;
+  this.column = 1;
+  this.hasHeader = true;
 };
 
 Fetch.prototype.getConfig = function getConfig() {
@@ -21,7 +24,6 @@ Fetch.prototype.query = function query(key) {
 
 Fetch.prototype.queryAll = function queryAll() {
   var config = this.getConfig();
-  Logger.log(config);
   for (var key in config) {
     if (!config.hasOwnProperty(key)) {
       continue;
@@ -36,8 +38,32 @@ Fetch.prototype.queryAll = function queryAll() {
 Fetch.prototype.queryByConfig = function queryByConfig(config) {
   var sf = Salesforce.getObject();
   var sheet = SpreadsheetApp.getActive().getSheetByName(config.SheetName);
-  var qb = (new SalesforceLib.FetchBuilder()).setupByParams(config.FetchBuilderParams);
-  (new Spreadsheet(sf, sheet, qb)).sync();
+  var qb = (new SalesforceLib.QueryBuilder()).setupByParams(config.FetchBuilderParams);
+  var fieldList = qb.fieldList;
+  var fieldNum = fieldList.length;
+  var row = this.row;
+  var column = this.column;
+
+  var records = sf.query(qb.getQuery());
+  if (records instanceof SalesforceLib.ResponseError) {
+    throw records;
+  }
+
+  if (this.hasHeader) {
+    sheet.getRange(row, column, 1, fieldNum).setValues([fieldList]);
+    row++;
+  }
+
+  records.forEach(function f(record) {
+    var values = [];
+    for (var i = 0; i < fieldNum; i++) {
+      values[values.length] = record.get(fieldList[i]);
+    }
+    sheet.getRange(row, column, 1, fieldNum).setValues([values]);
+    row++;
+  });
+
+  return this;
 };
 
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^fetchAll$" }] */
