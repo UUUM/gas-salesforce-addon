1138be8f2b487e3179569e9177e8b58616bbed03
Masato Bito <masato@bz2.jp>
2017-09-28 15:59:21 +0900

    add FetchTest class

---
+++ src/FetchTest.js
@@ -0,0 +1,34 @@
+testRunner.functions.push(function (test) {
+  (new ConfigQuery()).callback('{' +
+    '"Opportunity": {' +
+    '  "from": "Opportunity",' +
+    '  "fields": [' +
+    '    "Id",' +
+    '    "Name",' +
+    '    "Account.Id",' +
+    '    "Account.Name"' +
+    '  ],' +
+    '  "limit": 10' +
+    '}' +
+  '}');
+
+  test('new Fetch()', function (assert) {
+    var fetch = new Fetch();
+    assert.ok(fetch instanceof Fetch, 'creates a Fetch object');
+    assert.equal(fetch.column, 1, 'has a column property');
+    assert.ok(fetch.hasHeader, 'has a hasHeader property');
+    assert.equal(fetch.row, 1, 'has a row property');
+  });
+
+  test('Fetch.getConfig()', function (assert) {
+    var fetch = new Fetch();
+
+    var config1 = fetch.getConfig();
+    assert.ok(Obj.isObject(config1), 'returns an object');
+
+    var config2 = fetch.getConfig();
+    assert.equal(config1, config2, 'returns a same object');
+  });
+});
+
+/* eslint func-names: ["error", "never"] */
