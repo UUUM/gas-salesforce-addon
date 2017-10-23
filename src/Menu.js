5ea417c8ab608937bbb5a07a57942e98d44440e4
Masato Bito <masato@bz2.jp>
2017-10-23 15:11:56 +0900

    add Logout menu

--- src/Menu.js
+++ src/Menu.js
@@ -2,6 +2,11 @@ function sfAuthCallback(request) {
   return (new SFLib()).getOAuth2Client().callback(request);
 }
 
+function sfAuthReset() {
+  (new SFLib()).getOAuth2Client().reset();
+  SpreadsheetApp.getUi().alert('Logged out');
+}
+
 function sfAuthShow() {
   var html = SFLib.getInstance().getOAuth2Client().getAuthorizationHtml();
   SpreadsheetApp.getUi().showModalDialog(html, 'Authorization');
@@ -31,7 +36,8 @@ Menu.setup = function setup() {
     )
     .addItem('Fetch All', 'fetchAll')
     .addItem('Authenticate', 'sfAuthShow')
+    .addItem('Logout', 'sfAuthReset')
     .addToUi();
 };
 
-/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^sfAuth(Callback|Show)$" }] */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^sfAuth(Callback|Reset|Show)$" }] */
